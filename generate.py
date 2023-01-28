6d0eea5ab4707674b085e353f927115d8ab60ff1
Nick Carney <npcarney@uvm.edu>
2023-01-28 13:52:34 -0500

    Adding 10x10 sets of boxes

--- generate.py
+++ generate.py
@@ -1,10 +1,27 @@
 import pyrosim.pyrosim as pyrosim
 length=1
-width=2
-height = 3
-
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
-
+width=1
+height=1
+x=-.5
+y=0
+z=-.5
+pyrosim.Start_SDF("boxes.sdf")
+#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+#pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[length,width,height])
+adder=0
+for xc in range(10):
+    for yc in range(10):
+        for i in range(10):
+            adder+=height
+            print(height)
+            print((z+adder))
+            pyrosim.Send_Cube(name="Box", pos=[x+xc,y+yc,z+adder] , size=[length,width,height])
+            length*=.9
+            width*=.9
+            height*=.9
+        adder=0
+        length=1
+        width=1
+        height=1
 
 pyrosim.End()
