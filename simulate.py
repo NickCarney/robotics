a7bc2021214b83bed97d312e258e13e04b9a38ea
Nick Carney <npcarney@uvm.edu>
2023-02-06 20:51:51 -0500

    Adding first motor

--- simulate.py
+++ simulate.py
@@ -19,6 +19,12 @@ for i in range(iterations):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = b'Torso_BackLeg',
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = 0.0,
+    maxForce = 500)
     #print(frontLegSensorValues)
 np.save('backLegSensorData.npy',backLegSensorValues)
 np.save('frontLegSensorData.npy',frontLegSensorValues)
